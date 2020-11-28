0a8df6c5a0371ed1b89da7a4e6f09b82983cccfc
Gerry Sousa <gerry@ciandt.com>
2020-11-28 16:47:37 -0300

    5. Criando schemas do mongoose

---
+++ aula01/model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {type: String, require: true, unique: true, lowercase: true},
+    password: {type: String, require: true, select: false},
+    created: {type: Date, default: Date.now}    
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
