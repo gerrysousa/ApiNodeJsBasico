cc59a77da9d4afde3e57add3126b706892b44916
Gerry Sousa <gerry@ciandt.com>
2020-11-30 07:15:26 -0300

    9. Retornando status codes apropriados

--- aula01/middlewares/token.js
+++ aula01/middlewares/token.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const token = (req, res, next) => {
     const token_header = req.headers.token;
-    if(!token_header) return res.send({error: 'Token não enviado!'});
+    if(!token_header) return res.status(401).send({error: 'Token não enviado!'});
 
     jwt.verify(token_header, 'batatateste2020', (err, decoded) =>{
-        if(err) return res.send({error: 'Token invalido!'});
+        if(err) return res.status(401).send({error: 'Token invalido!'});
         res.locals.auth_data = decoded;
         return next();
     });
--- aula01/routes/users.js
+++ aula01/routes/users.js
@@ -17,44 +17,44 @@ router.get('/', async (req,res)=>{
         return res.send(users);
     }
     catch(err){
-        return res.send({error: 'Erro na consulta de usuarios!'});
+        return res.status(500).send({error: 'Erro na consulta de usuarios!'});
     }
 });
 
 router.post('/create', async (req,res)=>{
     const {email, password} = req.body;
-    if(!email || !password) return res.send({error: 'Dados Insuficientes!'});
+    if(!email || !password) return res.status(400).send({error: 'Dados Insuficientes!'});
 
     try {
-        if (await Users.findOne({email})) return res.send({error: 'Usuario ja registrado!'});
+        if (await Users.findOne({email})) return res.status(400).send({error: 'Usuario ja registrado!'});
         
         const user = await Users.create(req.body);
         user.password = undefined;
-        return res.send({user, token: createUserToken(user.id)}); 
+        return res.status(201).send({user, token: createUserToken(user.id)}); 
     } 
     catch (error) {
-        return res.send({error: 'Erro ao buscar usuario!'});
+        return res.status(500).send({error: 'Erro ao buscar usuario!'});
     }
 });
 
 router.post('/auth', async (req,res)=>{
     const {email, password} = req.body;
     
-    if(!email || !password) return res.send({error: 'Dados Insuficientes!'});
+    if(!email || !password) return res.status(400).send({error: 'Dados Insuficientes!'});
 
     try {
         const user = await Users.findOne({email}).select('+password');
-        if(!user) return res.send({error: 'Usuario nao registrado!'});
+        if(!user) return res.status(400).send({error: 'Usuario nao registrado!'});
 
         const pass_ok = await  bcrypt.compare(password, user.password);
 
-        if(!pass_ok) return res.send({error: 'Erro ao autenticar usuario!'});
+        if(!pass_ok) return res.status(401).send({error: 'Erro ao autenticar usuario!'});
 
         user.password = undefined;
         return res.send({user, token: createUserToken(user.id)});
     } 
     catch (error) {
-        return res.send({error: 'Erro ao buscar usuario!'});
+        return res.status(500).send({error: 'Erro ao buscar usuario!'});
     }    
 });
 
